a6b6fbe20ebe8a8cece7a68ad50f45db03d3c738
Asrul Sani Ariesandy <asrul.sani@koinworks.com>
2022-09-29 12:17:35 +0700

    Finished: api get url

--- klops/versioning/versioning.py
+++ klops/versioning/versioning.py
@@ -3,11 +3,9 @@ Main module for versioning Control.
 """
 
 import pickle
-from typing import Any, Dict, List, Union
+from typing import Any, List
 
 import dvc
-import numpy as np
-import pandas as pd
 
 from klops.config import LOGGER
 from klops.versioning.helper import shell_executor
@@ -29,7 +27,7 @@ class Versioning:
         """_summary_
         Add remote repository. Could be local, or remote storage such as GCP bucket or AWS s3.
         Args:
-            remote_url (str): _description_
+            remote_url (str): 
         """
         shell_executor(f"dvc remote add -d {name} {remote_url}")
 
@@ -37,7 +35,7 @@ class Versioning:
         """_summary_
         Track the file / path into DVC.
         Args:
-            file_or_path (str): _description_ File or path tobe added.
+            file_or_path (str):  File or path tobe added.
         """
         shell_executor(f"dvc add {file_or_path}")
 
@@ -67,10 +65,10 @@ class Versioning:
         """_summary_
         Run the defined DVC pipeline.
         Args:
-            entry_point (str): _description_ The main program to be executed.
-            name (str, optional): _description_. Defaults to None. The Pipeline name.
-            dependencies (List, optional): _description_. Defaults to []. List of dependencies. The same as `-d` options in dvc command.
-            outputs (List, optional): _description_. Defaults to []. List of the outputs, The same as `-o` options in dvc command.
+            entry_point (str):  The main program to be executed.
+            name (str, optional): . Defaults to None. The Pipeline name.
+            dependencies (List, optional): . Defaults to []. List of dependencies. The same as `-d` options in dvc command.
+            outputs (List, optional): . Defaults to []. List of the outputs, The same as `-o` options in dvc command.
         """
         deps = ""
         outs = ""
@@ -87,10 +85,10 @@ class Versioning:
         """_summary_
         Read the binary file such as .pkl, .joblib, etc. stored in the DVC storage / repository.
         Args:
-            file_name (str): _description_ The file name. Including its path.
+            file_name (str):  The file name. Including its path.
 
         Returns:
-            Any: _description_ Object pointer instances.
+            Any:  Object pointer instances.
         """
         try:
             model = pickle.loads(dvc.api.read(file_name, mode='rb'))
@@ -100,19 +98,40 @@ class Versioning:
         except dvc.exceptions.PathMissingError as path_missing:
             LOGGER.error(str(path_missing))
 
-    def read_dataset(self, file_name: str) -> Any:
+    def read_dataset(self, file_name: str, revision: str = None) -> Any:
         """_summary_
         Read dataset from DVC artifact storage.
         Args:
-            file_name (str): _description_ The file name. Including it's path.
+            file_name (str):  The file name. Including it's path.
 
         Returns:
-            Any: _description_ The dataset buffer. Need to parse.
+            Any:  The dataset buffer. Need to parse.
         """
         try:
-            with dvc.api.open(file_name) as file_buffer:
+            with dvc.api.open(file_name, rev=revision) as file_buffer:
                 return file_buffer
         except dvc.exceptions.FileMissingError as file_missing:
             LOGGER.error(str(file_missing))
         except dvc.exceptions.PathMissingError as path_missing:
             LOGGER.error(str(path_missing))
+
+    def get_url(self,
+                path: str,
+                repo: str = None,
+                rev: str = None,
+                remote: str = None) -> Any:
+        """Get the URL to the storage location of a data file or \
+            directory tracked in a DVC project.
+
+        Args:
+            path (str): 
+            repo (str, optional): . Defaults to None.
+            rev (str, optional): . Defaults to None.
+            remote (str, optional): . Defaults to None.
+
+        Returns:
+            Any: Returns the URL string of the storage location (in a DVC remote) \
+                where a target file or directory, specified by its path in a repo \
+                    (DVC project), is stored.
+        """
+        return dvc.api.get_url(path, repo, rev, remote)
